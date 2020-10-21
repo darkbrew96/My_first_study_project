c5a4576bc585935c33d1219fe7121ca8d24a89fa
DarkBrew <darkbrew96@gmail.com>
2020-10-21 18:53:04 +0300

    add logger messages

--- src/main/java/ru/stormnet/yandex/pages/loginPageEnvironment/LoginPage.java
+++ src/main/java/ru/stormnet/yandex/pages/loginPageEnvironment/LoginPage.java
@@ -1,41 +1,43 @@
-package ru.stormnet.yandex.pages;
+package ru.stormnet.yandex.pages.loginPageEnvironment;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import ru.stormnet.yandex.pages.AbstractPage;
 import ru.stormnet.yandex.utills.Expectant;
 
 public class LoginPage extends AbstractPage {
 
-    Expectant expectant = new Expectant(driver);
-
     private static final By USERNAME_FIELD_LOCATOR = By.cssSelector("#passp-field-login");
     private static final By LOGIN_BUTTON_LOCATOR = By.cssSelector("div.passp-sign-in-button.v5 > button");
     private static final By PASSWORD_FIELD_LOCATOR = By.cssSelector("#passp-field-passwd");
+    Expectant expectant = new Expectant(driver);
 
     public LoginPage(WebDriver driver) {
         super(driver);
 
     }
 
-    public WebElement getLoginField() {
+    private WebElement getLoginField() {
         return driver.findElement(USERNAME_FIELD_LOCATOR);
     }
 
-    public WebElement getPasswordField() {
+    private WebElement getPasswordField() {
         return driver.findElement(PASSWORD_FIELD_LOCATOR);
     }
 
-    public WebElement getButtonLocator() {
+    private WebElement getButtonLocator() {
         return driver.findElement(LOGIN_BUTTON_LOCATOR);
     }
 
-    public void loginWithCreeds(String username, String password) {//вернуть страницу профиля
+    public void loginWithCreeds(String username, String password) {
         expectant.waitingForAnElementOnThePage(USERNAME_FIELD_LOCATOR);
         getLoginField().sendKeys(username);
         getButtonLocator().click();
+        logger.info("username entered");
         expectant.waitingForAnElementOnThePage(PASSWORD_FIELD_LOCATOR);
         getPasswordField().sendKeys(password);
+        logger.info("password entered");
         expectant.waitingForAnElementOnThePage(LOGIN_BUTTON_LOCATOR);
         getButtonLocator().click();
     }
