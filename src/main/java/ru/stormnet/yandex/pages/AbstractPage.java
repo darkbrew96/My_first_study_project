19fb941a831e180b320640955aae561ea106f30e
DarkBrew <darkbrew96@gmail.com>
2020-10-21 18:46:47 +0300

    delete unused import

--- src/main/java/ru/stormnet/yandex/pages/AbstractPage.java
+++ src/main/java/ru/stormnet/yandex/pages/AbstractPage.java
@@ -3,8 +3,6 @@ package ru.stormnet.yandex.pages;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import ru.stormnet.yandex.utills.Expectant;
 
 public abstract class AbstractPage {
 
