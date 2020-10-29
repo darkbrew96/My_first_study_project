eea15818bc65b11c3d6d3b92e5844e119a60f78f
DarkBrew <darkbrew96@gmail.com>
2020-10-29 20:03:03 +0300

    add expectation for login button

--- src/main/java/ru/stormnet/yandex/pages/loginPageEnvironment/LoginPage.java
+++ src/main/java/ru/stormnet/yandex/pages/loginPageEnvironment/LoginPage.java
@@ -9,9 +9,8 @@ import ru.stormnet.yandex.utills.Expectant;
 public class LoginPage extends AbstractPage {
 
     private static final By USERNAME_FIELD_LOCATOR = By.cssSelector("#passp-field-login");
-    private static final By LOGIN_BUTTON_LOCATOR = By.cssSelector("div.passp-sign-in-button.v5 > button");
+    private static final By LOGIN_BUTTON_LOCATOR = By.cssSelector("div.passp-sign-in-button > button");
     private static final By PASSWORD_FIELD_LOCATOR = By.cssSelector("#passp-field-passwd");
-    Expectant expectant = new Expectant(driver);
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -27,18 +26,19 @@ public class LoginPage extends AbstractPage {
     }
 
     private WebElement getButtonLocator() {
+        new Expectant(driver).waitingForAnElementOnThePage(LOGIN_BUTTON_LOCATOR);
         return driver.findElement(LOGIN_BUTTON_LOCATOR);
     }
 
     public void loginWithCreeds(String username, String password) {
-        expectant.waitingForAnElementOnThePage(USERNAME_FIELD_LOCATOR);
+        new Expectant(driver).waitingForAnElementOnThePage(USERNAME_FIELD_LOCATOR);
         getLoginField().sendKeys(username);
         getButtonLocator().click();
         logger.info("username entered");
-        expectant.waitingForAnElementOnThePage(PASSWORD_FIELD_LOCATOR);
+        new Expectant(driver).waitingForAnElementOnThePage(PASSWORD_FIELD_LOCATOR);
         getPasswordField().sendKeys(password);
         logger.info("password entered");
-        expectant.waitingForAnElementOnThePage(LOGIN_BUTTON_LOCATOR);
+        new Expectant(driver).waitingForAnElementOnThePage(LOGIN_BUTTON_LOCATOR);
         getButtonLocator().click();
     }
 
