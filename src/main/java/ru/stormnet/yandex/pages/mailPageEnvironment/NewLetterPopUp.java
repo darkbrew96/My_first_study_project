4b6a1c65743cac2e9bfb02c7a21d3644afbde5db
DarkBrew <darkbrew96@gmail.com>
2020-10-29 20:07:35 +0300

    correct methods and log message

--- src/main/java/ru/stormnet/yandex/pages/mailPageEnvironment/NewLetterPopUp.java
+++ src/main/java/ru/stormnet/yandex/pages/mailPageEnvironment/NewLetterPopUp.java
@@ -4,19 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.stormnet.yandex.pages.AbstractPage;
-import ru.stormnet.yandex.pages.mailPageEnvironment.MailPage;
 import ru.stormnet.yandex.utills.Expectant;
-import ru.stormnet.yandex.utills.FileManager;
 import ru.stormnet.yandex.utills.PropertiesManager;
+
+import java.io.File;
 
 public class NewLetterPopUp extends MailPage {
 
     private static final By FILE_RECIPIENT_ADDRESS_FIELD_LOCATOR = By.cssSelector(".tst-field-to .composeYabbles");
     private static final By FILE_INPUT_LOCATOR = By.cssSelector(" div.composeReact__inner > div.composeReact__footer > div > div:nth-child(2) > div:nth-child(1) > input");
     private static final By LOADING_BAR_LOCATOR = By.cssSelector("div.ComposeAttachmentsLoadingProgress");
+    private static final By SEND_MESSAGE_BUTTON_LOCATOR = By.cssSelector("div.ComposeSendButton_desktop > button");
 
     public NewLetterPopUp(WebDriver driver) {
         super(driver);
@@ -34,25 +33,25 @@ public class NewLetterPopUp extends MailPage {
 
     }
 
-    public void attachFileToMessage() {
+    public void attachFileToMessage(File fileToSend) {
         WebElement fileInput = driver.findElement(FILE_INPUT_LOCATOR);
-        fileInput.sendKeys(FileManager.createFile().getAbsolutePath());
+        fileInput.sendKeys(fileToSend.getAbsolutePath());
     }
 
     public void sendMessageToRecipient() {
-        new Expectant(driver).waitingForAnElementOnThePage(By.cssSelector("div.ComposeSendButton_desktop > button"));
-        driver.findElement(By.cssSelector("div.ComposeSendButton_desktop > button")).click();
-        AbstractPage.logger.info("message sended to recipient!");
+        new Expectant(driver).waitingForAnElementOnThePage(SEND_MESSAGE_BUTTON_LOCATOR);
+        driver.findElement(SEND_MESSAGE_BUTTON_LOCATOR).click();
+        AbstractPage.logger.info("message sent to recipient!");
     }
 
     public boolean allFileIsAttached() {
         try {
-            (new WebDriverWait(driver, 10)).until(ExpectedConditions.invisibilityOfElementLocated(LOADING_BAR_LOCATOR));
+            new Expectant(driver).waitingForTheMissingElementOnThePage(LOADING_BAR_LOCATOR);
             AbstractPage.logger.info("all file is attached!");
             return true;
         } catch (TimeoutException ex) {
             System.out.println(ex.getMessage());
-            AbstractPage.logger.info("Failed to file attachments");
+            AbstractPage.logger.info("***Failed to file attachments***");
             return false;
         }
     }
